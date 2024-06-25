fa749512906c721a5a6d64b83a4676ee08c3bd82
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-24 18:53:40 -0700

    Commit

--- MapLocation/ViewController.swift
+++ MapLocation/ViewController.swift
@@ -6,14 +6,52 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
     }
-
-
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+           guard let location = locations.last else { return }
+           let coordinate = location.coordinate
+           
+           latitudeLabel.text = "Latitude: \(coordinate.latitude)"
+           longitudeLabel.text = "Longitude: \(coordinate.longitude)"
+           
+           let region = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+           mapView.setRegion(region, animated: true)
+           
+           let annotation = MKPointAnnotation()
+           annotation.coordinate = coordinate
+           annotation.title = "Your Location"
+           mapView.addAnnotation(annotation)
+           
+           manager.stopUpdatingLocation()
+       }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .denied {
+            print("Location access denied")
+        } else if status == .notDetermined {
+            print("Location status not determined")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager failed with error: \(error.localizedDescription)")
+    }
 }
-
