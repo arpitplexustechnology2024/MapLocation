f57d5cb0b02c7007b0f089317daeaf3999d767ac
arpitplexustechnology2024 <arpit.plexustechnology@gmail.com>
2024-06-25 14:31:12 -0700

    Commit

---
+++ MapLocation/NearbyLocationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NearbyLocationViewController.swift
+//  MapLocation
+//
+//  Created by Arpit iOS Dev. on 25/06/24.
+//
+
+import UIKit
+
+class NearbyLocationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
